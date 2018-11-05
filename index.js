3c9b41bf6015d74b3ba05bdfbfaafa1184d044bb
Szilvia Hosser-Cox <szilvia.hosser@gmail.com>
2018-11-05 20:52:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,10 +25,10 @@ function prependDriver(name) {
   return [name, ...drivers];
 }
 
-function removefirstDriver() {
-  return drivers.slice(1);
-}
-
 function removeLastDriver () {
   return drivers.slice(0, drivers.length - 1);
+}
+
+function removefirstDriver() {
+  return drivers.slice(1);
 }
